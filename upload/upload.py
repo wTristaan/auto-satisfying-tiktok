67e714a6376c9d50fe7042e12963d3645e8db956
wTristaan <t.joubert78980@gmail.com>
2025-06-14 16:52:01 +0200

    [FIX] driver

--- upload/upload.py
+++ upload/upload.py
@@ -1,13 +1,9 @@
-import os
-import time
-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from tiktok_uploader.upload import upload_video
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from utils.params import YOUTUBE_EMAIL, YOUTUBE_PASSWORD, DAILYMOTION_EMAIL, DAILYMOTION_PASSWORD, DAILYMOTION_API_KEY, DAILYMOTION_API_SECRET, DAILYMOTION_ID
+from utils.params import DAILYMOTION_EMAIL, DAILYMOTION_PASSWORD, DAILYMOTION_API_KEY, DAILYMOTION_API_SECRET, DAILYMOTION_ID
 from dailymotion_app.api import Dailymotion
 from youtube_app.api import YTApi
 
@@ -25,7 +21,13 @@ class Upload():
     def tiktok(self):
         try:
             self.rich_console.log("Uploading video to tiktok.")
-            upload_video(self.video_path, self.full_title, cookies=f'utils/cookies_{self.LANGUAGE.upper()}.txt', headless=True)
+            options = Options()
+            options.add_argument('--headless')
+            options.add_argument('--no-sandbox')
+            options.add_argument('--disable-dev-shm-usage')
+            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+            
+            upload_video(self.video_path, self.full_title, cookies=f'utils/cookies_{self.LANGUAGE.upper()}.txt', headless=True, browser_agent=driver)
             self.rich_console.log("Upload video to tiktok done.")
         except Exception as e:
             self.rich_console.log(f"Upload video to tiktok error {str(e)}.")
